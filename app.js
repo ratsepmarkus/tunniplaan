19c467a07ff6e5a68e5ee7483c2de73656fde118
ratsepmarkus <markusratsep11@gmail.com>
2020-10-01 19:17:43 +0300

    created dropdown menu for lessons. cleared up the code.

---
+++ app.js
@@ -0,0 +1,9 @@
+let dropdownBtn = document.querySelector('.menu-btn');
+let menuContent = document.querySelector('.menu-content');
+dropdownBtn.addEventListener('click',()=>{
+   if(menuContent.style.display===""){
+      menuContent.style.display="block";
+   } else {
+      menuContent.style.display="";
+   }
+})
